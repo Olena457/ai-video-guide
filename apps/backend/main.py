a197861b624af2638ce6b6fa0451b9477706b8bb
Olena457 <olenademianenko42@gmail.com>
2026-09-16 17:44:39 +0300

    chore: added new link

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
         "http://localhost:3000", 
         "http://127.0.0.1:3000", 
         "http://192.168.1.3:3000",
-        "https://frontend-seven-sage-81.vercel.app" 
+        "https://frontend-seven-sable-65.vercel.app" 
     ], 
     allow_credentials=True,
     allow_methods=["*"],
