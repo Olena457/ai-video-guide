e47e52a144b279401619f7226363d06c8bff821b
Olena457 <olenademianenko42@gmail.com>
2026-09-17 23:28:29 +0300

    chore: updated main.py

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,19 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers.guide import router
+
 app = FastAPI(title="Video AI Analysis API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000", 
-        "http://127.0.0.1:3000", 
-        "http://192.168.1.3:3000",
-        "https://frontend-seven-sable-65.vercel.app" 
-    ], 
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"], 
+    allow_origins=["*"],         
+    allow_credentials=False,      
+    allow_methods=["*"],          
+    allow_headers=["*"],         
 )
 
 app.include_router(router)
